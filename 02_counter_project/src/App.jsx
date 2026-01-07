eb8d4f36f60fdf4707fb73b27f4a498653ed2102
Prathamesh Mhettar <prathmeshmhettar@gmail.com>
2026-01-07 12:52:29 +0000

    new project

---
+++ 02_counter_project/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  const [counter , setCounter] = useState(15)
+
+  //let counter = 15;
+
+  const addValue = ()=>{
+
+    console.log("Clicked" , counter);
+    counter = counter + 1
+    setCounter()
+
+  }
+
+  return (
+    <>
+    
+     <h1>Chai Our React</h1>
+     <h2>Counter value : {counter}</h2>
+
+     <button onClick={addValue}>Add Value</button>
+     <br />
+     <button>Remove Value</button>
+    </>
+  )
+}
+
+export default App
